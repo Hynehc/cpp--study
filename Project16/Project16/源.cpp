a50d946ac1ba4fcd8315b24a56fc7cf30d512e7c
Vellichor-Chen <112534356+Vellichor-Chen@users.noreply.github.com>
2022-12-05 08:34:47 +0800

    Add files via upload

---
+++ Project16/Project16/源.cpp
@@ -0,0 +1,100 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<functional>
+#include<vector>
+//void print(int val)
+//{
+//	cout << val << " ";
+//}
+//class print1
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//void test()
+//{
+//	vector<int>v;
+//	for (int i = 0;i < 10;i++)
+//	{
+//		v.push_back(i);
+//	}
+//	for_each(v.begin(), v.end(), print);
+//	for_each(v.begin(), v.end(), print1());
+//	cout << endl;
+//}
+
+//class Transform
+//{
+//public:
+//	int operator()(int v)
+//	{
+//		return v;
+//	}
+//};
+//class Myprint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//
+//};
+//void test()
+//{
+//	vector<int>v;
+//	for (int i = 0;i < 10;i++)
+//	{
+//		v.push_back(i);
+//	}
+//	vector<int>vTarget;
+//	vTarget.resize(v.size());
+//	transform(v.begin(), v.end(), vTarget.begin(), Transform());
+//	for_each(vTarget.begin(), vTarget.end(), Myprint());
+//	cout << endl;
+//
+//}
+
+//class Greater
+//{
+//public:
+//	bool oerator()(int val)
+//	{
+//		return val > 20;
+//	}
+//};
+//void test()
+//{
+//	/*vector<int>v;
+//	v.push_back(10);
+//	vector<int>::iterator pos = adjacent_find(v.begin(), v.end());*/
+//
+//	vector<int>v;
+//	//for (int i = 0;i < 10;i++)
+//	//{
+//	//	v.push_back(i);
+//	//}
+//	////binary_search(v.begin(), v.end(), 9);
+//	//int num = count(v, begin(), v.end(), 5);
+//	v.push_back(10);
+//	v.push_back(20);
+//	v.push_back(30);
+//	count_if(v.begin(), v.end(), Greater());
+//
+//
+//}
+
+void test()
+{
+
+}
+int main()
+{
+	test();
+	return 0;
+}
